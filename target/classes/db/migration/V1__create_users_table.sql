c8ba34c96ffecf7159fe3dd33826d10f8ced4896
qubby <@vydrenkoyegor@gmail.com>
2025-03-12 16:04:42 +0300

    Remove target and .idea folders

--- target/classes/db/migration/V1__create_users_table.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE users (
-                       id SERIAL PRIMARY KEY,
-                       username VARCHAR(255) NOT NULL UNIQUE,
-                       password VARCHAR(255) NOT NULL
-);
-
-CREATE INDEX username_index ON users (username);
